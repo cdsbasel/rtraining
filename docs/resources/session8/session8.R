9470e10d7a865e411d3a4ae05df4c8bd1a05e25d
gayoungson <gayoungson94@gmail.com>
2021-08-15 22:55:37 +0900

    update

---
+++ docs/resources/session8/session8.R
@@ -0,0 +1,21 @@
+### R Training
+### Session 8: Reshape Data
+### 12.08.21
+
+# load packages individually, in order to load the "billboard" dataset
+library(tidyr)
+library(dplyr)
+
+billboard_longer <- billboard %>% 
+  pivot_longer(
+    cols = starts_with("wk"), 
+    names_to = "week", 
+    names_prefix = "wk",
+    values_to = "rank",
+    values_drop_na = TRUE,
+  )
+
+billboard_longer %>%
+  group_by(track) %>%
+  summarise(average = mean(rank)) %>%
+  arrange(average)
